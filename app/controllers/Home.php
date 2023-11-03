e293a796ae56703a65fd33b155e9636ed9e4e39c
Akmal Rafi Diara Putra <49126626+akmalrafidiara@users.noreply.github.com>
2023-11-03 16:09:34 +0700

    add title

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -4,6 +4,9 @@ class Home extends Controller
 {
   public function index()
   {
+    $data = [
+      'title' => 'Home Page'
+    ];
     $this->getHeader($data);
     $this->view('home/index', $data);
     $this->getFooter();
